4d0eaf6001c1537dc547af721c0f1daa4ae98bd9
Pedro Mendoza <pedro.mendoza.b14@gmail.com>
2024-04-30 22:59:50 -0600

    change: se modificaron los alerts de registro

--- src/assets/js/SignUp.js
+++ src/assets/js/SignUp.js
@@ -10,45 +10,63 @@ signupForm.addEventListener("submit", (e) => {
   // Validación del nombre
   const nameRegex = /^[a-zA-Z\s]+$/; // Solo acepta letras y espacios
   if (!nameRegex.test(name.value)) {
+    name.classList.add("is-invalid");
     return showAlert(
       "Por favor, introduce un nombre válido (solo letras)",
       name
     );
+  } else {
+    name.classList.remove("is-invalid");
   }
 
   // Validación de correo electrónico
   const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   if (!emailRegex.test(email.value)) {
+    email.classList.add("is-invalid");
     return showAlert(
       "Por favor, introduce un correo electrónico válido",
       email
     );
+  } else {
+    email.classList.remove("is-invalid");
   }
 
   // Validación del teléfono
   const phoneRegex = /^\d{10}$/; // Asume que el número de teléfono tiene 10 dígitos
   if (!phoneRegex.test(number.value)) {
+    number.classList.add("is-invalid");
     return showAlert(
       "Por favor, introduce un número de teléfono válido",
       number
     );
+  } else {
+    number.classList.remove("is-invalid");
   }
 
   // Contraseña coincide
   if (password.value !== password1.value) {
+    password1.classList.add("is-invalid");
     return showAlert("Las contraseñas no coinciden", password1);
+  } else {
+    password1.classList.remove("is-invalid");
   }
 
   const Users = JSON.parse(localStorage.getItem("users")) || [];
   const isUserRegistered = Users.find((user) => user.email === email.value);
   if (isUserRegistered) {
+    email.classList.add("is-invalid");
     return showAlert("El usuario ya esta registrado", email);
+  } else {
+    email.classList.remove("is-invalid");
   }
 
   // Verificación de número de teléfono
   const isNumberRegistered = Users.find((user) => user.number === number.value);
   if (isNumberRegistered) {
+    number.classList.add("is-invalid");
     return showAlert("El número de teléfono ya está registrado", number);
+  } else {
+    number.classList.remove("is-invalid");
   }
 
   Users.push({
@@ -76,4 +94,14 @@ function showAlert(message, element, type = "danger") {
   element.insertAdjacentHTML("beforebegin", alertTemplate);
   const alertElement = document.querySelector(".alert");
   alertElement.scrollIntoView({ behavior: "smooth" });
+
+  // Cambiar el color de los campos directamente con JavaScript
+  if (type === "danger") {
+    //element.style.borderColor = "red";
+  } else {
+    const formElements = signupForm.querySelectorAll("input");
+    formElements.forEach((el) => {
+      el.style.borderColor = "green";
+    });
+  }
 }
